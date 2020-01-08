0496936aec721f62511cfacd4f0f8989373a7236
Ariel Huynh <arielhuynh@ymail.com>
2020-01-07 20:01:20 -0800

    checking to see if screenshot worked

--- java.js
+++ java.js
@@ -1,33 +1,5 @@
-var upper = "ABCDEFHGIJKLMNOPQRSTUVWXYZ";
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var number = "0123456789";
-var symbol = "!@#$%^&*";
-function genPwd() {
-    var chars = '';
-    var password = '';
-    var len = document.getElementById("length").value;
-
-    if ( document.getElementById("uppercase").checked ) {
-        chars += upper;
-    }
-    if ( document.getElementById("lowercase").checked ) {
-        chars += lower;
-    }
-    if ( document.getElementById("numbers").checked ) {
-        chars += number;
-    }
-    if ( document.getElementById("symbols").checked ) {
-        chars += symbol;
-    }
-
-    if ( chars == '' ) {
-        password = 'Please select criteria(s)';
-    }
-    else {
-        for ( var i = 0; i < len; ++i ) {
-            password += chars[Math.floor(Math.random() * chars.length)];
-            }
-    }
-
-    document.getElementById("password").value = password;
-}
+var length = prompt("Choose password length between 8-128");
+var specChar = confirm("Do you want to use special characters?");
+var numChar = confirm("Do you want to use numbers?");
+var lowChar = confirm("Do you want to use lowercase letters?");
+var uppChar = confirm("Do you want to use uppercase letters?");
