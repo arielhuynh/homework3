a7129a1132bae96f17eb2dde5e096fa5818295ad
Ariel Huynh <arielhuynh@ymail.com>
2020-01-10 20:16:19 -0800

    made changes so that the crieria be prompts instead of checkboxes

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Unit 03 JavaScript Homework: Password Generator
 
 Description
 
-This application is used to generate a random password, dependent on the criteria(s) selected by the end user. If the end user does not pick any criterias, they will get a notification that tells them they need to pick a criteria in order for a password to be generated. 
+This application is used to generate a random password, dependent on the criteria(s) selected by the end user. The end user is prompted to enter the length of their password, ranging from 8-128. They also are prompted to confirm which criteria's they woud like. Once all that information is entered, the end user can git generate and a random password string that matches the criteria the end user entered will pop up. 
 
 ![screenshot](./assets/needcriteria.JPG)
+
+The link to the website is: https://arielhuynh.github.io/homework3/.
--- java.js
+++ java.js
@@ -1,5 +1,88 @@
-var length = prompt("Choose password length between 8-128");
-var specChar = confirm("Do you want to use special characters?");
-var numChar = confirm("Do you want to use numbers?");
-var lowChar = confirm("Do you want to use lowercase letters?");
-var uppChar = confirm("Do you want to use uppercase letters?");
+window.addEventListener('load', function() {
+    var length = prompt("Please choose password length between 8-128 characters.");
+  
+    while (length < 8 || length > 128) {
+      length = prompt("Please choose a number between 8-128!!");
+    }
+    var symChar = confirm("Do you want to use symbols?");
+    var numChar = confirm("Do you want to use numbers?");
+    var lowChar = confirm("Do you want to use lowercase letters?");
+    var uppChar = confirm("Do you want to use uppercase letters?");
+    
+
+    while (!(uppChar || lowChar || numChar || symChar)) {
+      alert("Choose at least one criteria!!");
+  
+      symChar = confirm("Do you want to use symbols?");
+      numChar = confirm("Do you want to use numbers?");
+      lowChar = confirm("Do you want to use lowercase letters?");
+      uppChar = confirm("Do you want to use uppercase letters?");
+      
+    }
+  
+    const results = document.getElementById('password');
+  
+    document.getElementById('generate').addEventListener('click', () => {
+      results.value = genPassword(lowChar, uppChar, numChar, symChar, length);
+    });
+  
+
+  
+      if (!password) {
+        return;
+      }
+  
+      
+  });
+  
+  
+  const randFun = {
+    symbol: randSym,
+    number: randNum,
+    lower: randLow,
+    upper: randUpp
+  
+  };
+  
+  function genPassword(lower, upper, number, symbol, length) {
+    let generatedPass = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{
+      lower
+    }, {
+      upper
+    }, {
+      number
+    }, {
+      symbol
+    }].filter(item => Object.values(item)[0]);
+  
+   
+    for (let i = 0; i < length; i += typesCount) {
+      typesArr.forEach(type => {
+        const funcName = Object.keys(type)[0];
+        generatedPass += randFun[funcName]();
+      });
+    }
+  
+    const finalPassword = generatedPass.slice(0, length);
+  
+    return finalPassword;
+  }
+
+  function randLow() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function randUpp() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function randNum() {
+	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function randSym() {
+	const symbols = '!@#$%^&*'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
